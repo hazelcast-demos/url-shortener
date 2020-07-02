f402bd6db335d64e925200968b0735da4dccf976
Nicolas Frankel <nicolas@frankel.ch>
2020-07-02 10:41:27 +0200

    Cleanup

--- src/main/kotlin/org/hazelcast/urlshrtn/ExampleResource.kt
+++ src/main/kotlin/org/hazelcast/urlshrtn/ExampleResource.kt
@@ -1,8 +1,6 @@
 package org.hazelcast.urlshrtn
 
-import javax.ws.rs.GET
-import javax.ws.rs.Path
-import javax.ws.rs.Produces
+import javax.ws.rs.*
 import javax.ws.rs.core.MediaType
 
 @Path("/hello")
