f830a9b76970f69f23e37bf8755d13195cdb36fc
Nicolas Frankel <nicolas@frankel.ch>
2020-07-02 12:06:48 +0200

    Create short URL out of long ones

---
+++ src/test/kotlin/org/hazelcast/urlshrtn/RestApiTest.kt
@@ -0,0 +1,34 @@
+package org.hazelcast.urlshrtn
+
+import io.quarkus.test.junit.QuarkusTest
+import io.restassured.RestAssured.given
+import org.hamcrest.core.StringStartsWith.startsWith
+import org.junit.jupiter.api.Test
+
+@QuarkusTest
+class RestApiTest {
+
+    private val urlParamName = "url"
+    private val longUrl = "http://foo.bar"
+    private val endpoint = "/{${urlParamName}}"
+
+    @Test
+    fun `POST should return response with HTTP status 200`() {
+        whenPost()
+        .then()
+            .statusCode(200)
+    }
+
+    @Test
+    fun `POST should return response whose content starts with the root url`() {
+        whenPost()
+        .then()
+            .body(startsWith(PREFIX))
+    }
+
+    private fun whenPost() =
+        given()
+            .pathParam(urlParamName, longUrl)
+        .`when`()
+            .post(endpoint)
+    }
