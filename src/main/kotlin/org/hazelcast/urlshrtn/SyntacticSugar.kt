54b1935a715748d10eff741c69c9097be366254b
Nicolas Frankel <nicolas@frankel.ch>
2020-07-06 16:44:04 +0200

    Improve code with Kotlin's syntactic sugar

---
+++ src/main/kotlin/org/hazelcast/urlshrtn/SyntacticSugar.kt
@@ -0,0 +1,10 @@
+package org.hazelcast.urlshrtn
+
+import com.hazelcast.core.EntryEvent
+import com.hazelcast.map.IMap
+import com.hazelcast.map.listener.EntryAddedListener
+import java.util.*
+
+fun <K, V> IMap<K, V>.addEntryAddedListener(includeValue: Boolean = true,
+                                            listener: (EntryEvent<K, V>) -> Unit): UUID =
+    addEntryListener(EntryAddedListener(listener), includeValue)
